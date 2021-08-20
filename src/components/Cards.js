241c7cb4e3daa2e8ab7574d7e3f2c7658a4d4284
Charith <wcharithbuddhipriya@gmail.com>
2021-08-20 23:29:26 +0530

    add gallery to card.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Cards.css';
 import CardItem from './CardItem';
+import './Gallery.css';
 
 function Cards() {
   return (
@@ -53,6 +54,47 @@ function Cards() {
           </ul>
         </div>
       </div>
+
+      {/* Gallery Code */}
+
+      <div className="Gallery">
+        <div className="row">
+          <div className="headTop">
+            <div className="row">
+              <div className="col" style={{ textAlign: 'center'}}>
+                <h1>Travel Gallery</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">          
+          <div className="col-12">
+            <div className="col shadow">
+              <div className="box" style={{ maxWidth: '100%' }}>
+                <div className="dream">
+                  <img src="/images/acc-gallery-8.jpg" />
+                  <img src="/images/acc-gallery-1.jpg" />
+                  <img src="/images/acc-gallery-9.jpg" />
+                </div>
+                <div className="dream">
+                  <img src="/images/acc-gallery-3.jpg" />
+                  <img src="/images/acc-gallery-0.jpg" />
+                </div>
+                <div className="dream">
+                  <img src="/images/acc-gallery-2.jpg" />
+                  <img src="/images/acc-gallery-12.jpg" />
+                  <img src="/images/acc-gallery-11.jpg" />
+                </div>
+                <div className="dream">
+                  <img src="/images/acc-gallery-14.jpg" />
+                  <img src="/images/acc-gallery-5.jpg" />
+                  <img src="/images/acc-gallery-7.jpg" />
+                </div>
+              </div>
+            </div>
+          </div>          
+        </div>
+      </div>      
     </div>
   );
 }
